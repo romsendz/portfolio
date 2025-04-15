574b8881e48e144f9194c2a17f605db2f756ae8b
romsendz <sendzyuk.r@gmail.com>
2025-04-15 14:59:55 +0200

    built projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -36,6 +36,129 @@ const projects = [
     ],
   },
   {
+    title: `ugcc-pokrov-v2`,
+    description: (
+      <>
+        <p>
+          A full-stack web application rebuilt for the UGCC Church in my lovely
+          hometown -{" "}
+          <Link
+            href={"https://maps.app.goo.gl/3sw5twypAqMgWVoZA"}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="inline-flex items-center gap-1 text-blue-500 hover:text-blue-700"
+          >
+            Zalishchyky
+            <ExternalLinkIcon className="w-4" />
+          </Link>
+          .
+        </p>
+        <br />
+        <p>
+          This is a complete rewrite of the{" "}
+          <Link
+            className="text-blue-500 hover:text-blue-700"
+            href={"#ugcc-pokrov"}
+          >
+            {" "}
+            original project
+          </Link>{" "}
+          with a new design and improved performance. Designed to provide a
+          modern, user-friendly platform for the local community to access
+          church-related information. Features a CMS-like structure allowing
+          authenticated admins to update content dynamically. Implements
+          NextAuth with credential-based authentication for secure admin access.
+          Fully responsive design ensures accessibility across all devices.
+        </p>
+        <br />
+        <p>
+          The goal is to strengthen the connection between the church and the
+          community by providing a centralized hub for updates and resources.
+        </p>
+      </>
+    ),
+    link: `https://ugcc-pokrov.te.ua`,
+    archived: false,
+    img: `/ugcc-pokrov-v2.png`,
+    technologies: [
+      `React`,
+      `Next.js`,
+      `TypeScript`,
+      `Tailwind CSS`,
+      `SASS/SCSS`,
+      `Prisma ORM`,
+      `Supabase (PostgreSQL)`,
+      `Tanstak Query`,
+      `React Context API`,
+      `NextAuth.js`,
+      `React Hook Form`,
+      `Zod`,
+      `Rest API`,
+      `Responsive Design`,
+      `CSR`,
+      `SSR`,
+    ],
+  },
+  {
+    title: `hp-world`,
+    description: (
+      <>
+        <p>
+          HP World is a fully responsive web application, designed to provide a
+          basic wiki information about the Harry Potter universe. Utilized{" "}
+          <Link
+            href={"https://github.com/fedeperin/potterapi"}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="inline-flex items-center gap-1 text-blue-500 hover:text-blue-700"
+          >
+            PotterAPI by fedeperin
+            <ExternalLinkIcon className="w-4" />
+          </Link>{" "}
+          as a data / API source.
+        </p>
+        <br />
+        <p>
+          Optimized for all devices, from mobile to desktop, using a
+          mobile-first approach.
+        </p>
+        <p>
+          Supports 6 languages: English, Español, Français, Italiano, Português,
+          Українська.
+        </p>
+        <p>
+          Includes pages for exploring books, characters, houses, and spells
+          from the Harry Potter universe, with data fetched dynamically from an
+          external API. Built with React and TypeScript for reusable, modular,
+          and type-safe components. Powered by React Query for efficient data
+          fetching, caching, and synchronization.
+        </p>
+        <br />
+        <p>
+          The goal of this project was to play around with status-quo tools and
+          features while building a functional and visually appealing React
+          application. It served as a playground to explore different tools and
+          approaches.
+        </p>
+      </>
+    ),
+    link: `https://hp-world.vercel.app/`,
+    archived: false,
+    img: `/hp-world.png`,
+    technologies: [
+      `React`,
+      `TypeScript`,
+      `CSS`,
+      `SASS/SCSS`,
+      `Tanstak Query`,
+      `i18next`,
+      `Vite`,
+      `GitHub Actions (CI/CD)`,
+      `Responsive Design`,
+      `Rest API`,
+    ],
+  },
+  {
     title: `ugcc-pokrov`,
     description: (
       <>
@@ -85,63 +208,6 @@ const projects = [
       "GCloud",
     ],
   },
-  {
-    title: `ugcc-pokrov-v2`,
-    description: (
-      <>
-        <p>
-          A full-stack web application rebuilt for the UGCC Church in my lovely
-          hometown -{" "}
-          <Link
-            href={"https://maps.app.goo.gl/3sw5twypAqMgWVoZA"}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="inline-flex items-center gap-1 text-blue-500 hover:text-blue-700"
-          >
-            Zalishchyky
-            <ExternalLinkIcon className="w-4" />
-          </Link>
-          .
-        </p>
-        <br />
-        <p>
-          This is a complete rewrite of the original project with a new design
-          and improved performance. Designed to provide a modern, user-friendly
-          platform for the local community to access church-related information.
-          Features a CMS-like structure allowing authenticated admins to update
-          content dynamically. Implements NextAuth with credential-based
-          authentication for secure admin access. Fully responsive design
-          ensures accessibility across all devices.
-        </p>
-        <br />
-        <p>
-          The goal is to strengthen the connection between the church and the
-          community by providing a centralized hub for updates and resources.
-        </p>
-      </>
-    ),
-    link: `https://ugcc-pokrov.te.ua`,
-    archived: false,
-    img: `/ugcc-pokrov-v2.png`,
-    technologies: [
-      `React`,
-      `Next.js`,
-      `TypeScript`,
-      `Tailwind CSS`,
-      `SASS/SCSS`,
-      `Prisma ORM`,
-      `Supabase (PostgreSQL)`,
-      `Tanstak-Query`,
-      `React Context API`,
-      `NextAuth.js`,
-      "React Hook Form",
-      `Zod`,
-      `Rest API`,
-      `Responsive Design`,
-      `CSR`,
-      `SSR`,
-    ],
-  },
 ];
 
 const Page = () => {
@@ -153,7 +219,7 @@ const Page = () => {
             id={project.title}
             key={project.title}
             className={clsx(
-              "flex backdrop-filter backdrop-blur-[100px] flex-row w-full px-8 py-12 bg-transparent gap-12 relative hover:opacity-100 transition-all duration-200",
+              "flex backdrop-filter text-center lg:text-start backdrop-blur-[100px] flex-col lg:flex-row w-full px-8 py-12 bg-transparent gap-12 relative hover:opacity-100 transition-all duration-200",
               { ["opacity-50"]: project.archived }
             )}
             spotlightColor="rgba(168, 141, 235, 0.14)"
@@ -195,6 +261,7 @@ const Page = () => {
                 asChild
                 title={project.archived ? "Preview (archived)" : "Preview"}
                 variant={project.archived ? "ghost" : "default"}
+                className="hidden lg:block"
               >
                 <Link
                   target="_blank"
@@ -205,6 +272,21 @@ const Page = () => {
                 </Link>
               </Button>
             </div>
+            <Button
+              asChild
+              title={project.archived ? "Preview (archived)" : "Preview"}
+              className="block lg:hidden"
+            >
+              <Link
+                target="_blank"
+                rel="noopener noreferrer"
+                href={project.link}
+                className="inline-flex items-center gap-2"
+              >
+                <MonitorIcon />
+                {project.archived ? "Preview (archived)" : "Preview"}
+              </Link>
+            </Button>
           </SpotlightCard>
         );
       })}
