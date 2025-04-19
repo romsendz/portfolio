740c945508e2f6f60c13a0eb4a2ec50ba823bbb7
romsendz <sendzyuk.r@gmail.com>
2025-04-19 10:39:36 +0200

    removed comments

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,34 +3,3 @@ const Page = () => {
 };
 
 export default Page;
-
-/*
-
-Якщо ви відкрили цю вкладку і ви рекрутер, то вам варто зупинитись. Якщо ви гарний 'рекрутер' і вам цікаво дізнатись більше про мене, то тепер ми друзі! 
-
-Ти тепер - мій друг, а я твій друг.
-Дякую за це! :)
-
-Це мій сайт, тут я розповідаю про себе, свої проекти та навички. Це все звісно цікаво, але я відчуваю, що хочу вам розповісти щось більше.
-
-
-King gizzrd & Lizzrd Wizaard
-
-
-.... ... ..
-
-Напевно ти зараз думаєш, "чому я ч8итаю це, навіщо мені це потрібно?". no shit you do!. But, wait. Here am I to tell about the most incrediable movie, that the univerce created. And look sis, you probably have not seen it. Intriged? I am!
-
-My Thoughts Are Silent (2019).
-
-Of cource I am biased. Of cource I've watched this one 6 times (I think). 
-
-(відсилка до рекрутера: Ох, ти досі це читаєш! Тепер ми навіть щось більше ніж друзі!)
-
-
-
-`sweet, harmonious, vibe, but страждальну (why) sad(!) Ukraine`
-
-
-
-*/
